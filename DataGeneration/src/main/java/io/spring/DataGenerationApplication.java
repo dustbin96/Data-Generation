ac5345ddb01daa3b8311be62473f68ca3497be0b
Kennard <dustbin96@gmail.com>
2023-01-11 17:39:51 +0800

    Create Project

---
+++ DataGeneration/src/main/java/io/spring/DataGenerationApplication.java
@@ -0,0 +1,13 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataGenerationApplication.class, args);
+	}
+
+}
